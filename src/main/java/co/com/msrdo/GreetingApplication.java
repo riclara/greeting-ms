1e62b5c00762b5120aa059b07afb1035db6d8804
Ricardo Lara <riclara@gmail.com>
2017-01-20 07:43:01 -0500

    configure domain name

--- src/main/java/co/com/msrdo/GreetingApplication.java
+++ src/main/java/co/com/msrdo/GreetingApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableAutoConfiguration
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
+@EnableEurekaClient
 public class GreetingApplication {
 
 	public static void main(String[] args) {
