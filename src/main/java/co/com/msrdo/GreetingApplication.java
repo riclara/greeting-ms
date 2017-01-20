3aae1176b9451439be6d8a4f44914cf1e5e7c9af
Ricardo Lara <riclara@gmail.com>
2017-01-20 08:21:32 -0500

    comment port

--- src/main/java/co/com/msrdo/GreetingApplication.java
+++ src/main/java/co/com/msrdo/GreetingApplication.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableAutoConfiguration
 @SpringBootApplication
-//@EnableDiscoveryClient
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class GreetingApplication {
 
 	public static void main(String[] args) {
