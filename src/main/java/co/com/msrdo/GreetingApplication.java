96c095e66a5617809f6e8e27083157e8de7a5188
Ricardo Lara <riclara@gmail.com>
2017-01-20 10:08:40 -0500

    test config

--- src/main/java/co/com/msrdo/GreetingApplication.java
+++ src/main/java/co/com/msrdo/GreetingApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @EnableAutoConfiguration
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class GreetingApplication {
 
 	public static void main(String[] args) {
